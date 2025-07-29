baa0795250d472a01b11cd273a7f4d9ecbf9977e
lawrencelonghi <lawlonghi@gmail.com>
2025-07-29 11:27:36 -0300

    navbar update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,13 +11,13 @@ export const Navbar = ({ menuOpen, setMenuOpen }) => {
 <nav className="fixed top-0 left-0 md:top-8 w-full z-40 bg-[rgba(255,255,255,0.2)] backdrop-blur-xs">
   <div className="flex ml-8 mr-8 items-center justify-between max-w-7xl  h-16 ">
     <div className="space-x-2">
-      <a href="#work" className="pr-2 text-2xl sm:text-2xl md:text-3xl text-gray-700 ">Rick Tadeu</a>
-      <span className=" sm:text-2xl text-gray-600 sm:ml-4 ">beauty artist</span>
-      </div>
+      <a href="#work" className="pr-2 font-bold text-1xl sm:text-2xl md:text-3xl text-black tracking-widest">RICK TADEU</a>
+      <span className=" font-bold text-1xl sm:text-2xl md:text-3xl text-black tracking-widest sm:ml-4 ">BEAUTY ARTIST</span>
+    </div>
 
 {!menuOpen && (
   <div
-    className="relative cursor-pointer text-gray-950 z-40 md:hidden text-2xl"
+    className="relative cursor-pointer text-gray-950 z-40 lg:hidden text-2xl"
     onClick={() => setMenuOpen(true)}
   >
     &#9776;
@@ -25,10 +25,10 @@ export const Navbar = ({ menuOpen, setMenuOpen }) => {
 )}
 
 
-      <div className="hidden md:flex gap-14">
-        <a href="#work" className="text-xl text-gray-800 tracking-wider hover:text-gray-700 transition-colors">Work</a>
-        <a href="#about" className="text-xl text-gray-800 tracking-wider hover:text-gray-700 transition-colors">About</a>
-        <a href="#contact" className="text-xl text-gray-800 tracking-widest hover:text-gray-700 transition-colors">Contact</a>
+      <div className="hidden lg:flex md:gap-14">
+        <a href="#work" className="text-xl text-gray-950 tracking-widest hover:text-gray-500 transition-colors underline-animation">Work</a>
+        <a href="#about" className="text-xl text-gray-950 tracking-widest hover:text-gray-500 transition-colors underline-animation">About</a>
+        <a href="#contact" className="text-xl text-gray-950 tracking-widest hover:text-gray-500 transition-colors underline-animation">Contact</a>
       </div>
 
     </div>
